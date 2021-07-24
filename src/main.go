5a26a772ffdf02c8ff97a8e87414237c553882c8
PoorlyDefinedBehaviour <brunotj2015@hotmail.com>
2021-07-24 20:59:34 -0300

    chore: remove test variables

--- src/main.go
+++ src/main.go
@@ -2,14 +2,12 @@ package main
 
 import (
 	"log"
-	"time"
 
 	"github.com/joho/godotenv"
 	"github.com/labstack/echo/v4"
 	config "oauth2_example/src/config"
 	authrouter "oauth2_example/src/http/routers/auth"
 	homerouter "oauth2_example/src/http/routers/home"
-	inmemorycache "oauth2_example/src/infra/in_memory_cache"
 )
 
 func main() {
@@ -25,10 +23,6 @@ func main() {
 
 	authrouter.RegisterRoutes(e)
 	homerouter.RegisterRoutes(e)
-
-	cache := inmemorycache.New()
-
-	cache.SetWithExpiration("key", "value", 1*time.Minute)
 
 	if err := e.Start(":3000"); err != nil {
 		log.Fatal(err)
