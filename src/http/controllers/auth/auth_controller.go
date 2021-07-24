a91f51092ce5b701ce1b902fa59f28d532755d3a
PoorlyDefinedBehaviour <brunotj2015@hotmail.com>
2021-07-24 18:37:55 -0300

    feat(controllers): create auth controller

--- src/http/controllers/auth/auth_controller.go
+++ src/http/controllers/auth/auth_controller.go
@@ -1,11 +1,32 @@
 package authcontroller
 
-import "github.com/labstack/echo/v4"
+import (
+	"net/http"
+
+	authservice "oauth2_example/src/domain/services/auth"
+
+	"github.com/labstack/echo/v4"
+)
 
 func Signin(ctx echo.Context) error {
-	return nil
+	return ctx.Redirect(http.StatusTemporaryRedirect, authservice.GenerateOAuth2URL())
 }
 
 func OAuth2Callback(ctx echo.Context) error {
-	return nil
+	code := ctx.QueryParam("code")
+	if code == "" {
+		return ctx.Redirect(http.StatusTemporaryRedirect, "/")
+	}
+
+	state := ctx.QueryParam("state")
+	if state == "" {
+		return ctx.Redirect(http.StatusTemporaryRedirect, "/")
+	}
+
+	userInfo, err := authservice.HandleOAuth2Callback(code, state)
+	if err != nil {
+		return ctx.NoContent(http.StatusInternalServerError)
+	}
+
+	return ctx.JSON(http.StatusOK, userInfo)
 }
