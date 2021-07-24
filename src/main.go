b273f3fd2321b765c14bf7a0fc73bc8fcf58f099
PoorlyDefinedBehaviour <brunotj2015@hotmail.com>
2021-07-24 18:38:05 -0300

    feat: start server

--- src/main.go
+++ src/main.go
@@ -1,12 +1,30 @@
 package main
 
 import (
+	"log"
+
+	"github.com/joho/godotenv"
 	"github.com/labstack/echo/v4"
+	config "oauth2_example/src/config"
 	authrouter "oauth2_example/src/http/routers/auth"
+	homerouter "oauth2_example/src/http/routers/home"
 )
 
 func main() {
+	if err := godotenv.Load(); err != nil {
+		log.Fatal(err)
+	}
+
+	if err := config.ReadConfigsFromEnv(); err != nil {
+		log.Fatal(err)
+	}
+
 	e := echo.New()
 
 	authrouter.RegisterRoutes(e)
+	homerouter.RegisterRoutes(e)
+
+	if err := e.Start(":3000"); err != nil {
+		log.Fatal(err)
+	}
 }
